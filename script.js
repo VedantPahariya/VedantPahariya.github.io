144f7842f11a0055c280ea8f8b220346a3b86968
Vedant Pahariya <vedant.pahariya@research.iiit.ac.in>
2024-03-09 21:10:02 +0530

    updated the animation

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+// Starting Animation
+document.addEventListener('DOMContentLoaded', function() {
+  const image = document.querySelector('.animated-image');
+  setTimeout(function() {
+        image.classList.add('animatze'); // Add the class to start the animation
+      }, 2000); 
+//   image.style.animation = 'image-animation 2s ease-in-out forwards';
+  setTimeout(function() {
+    content.style.opacity = '1'; // Show the content gradually after animation ends
+  }, 3400); // Delay the content display for the same duration as the animation
+});
+// Animation ends here
+
 const changingTexts = ["all your tech needs!", "your Laptops", "your PCs"];
 let textIndex = 0;
 let charIndex = 0;
@@ -33,3 +46,29 @@ document.addEventListener('DOMContentLoaded', () => {
   const cursorElement = document.getElementById('cursor');
   cursorElement.style.display = 'inline-block';
 });
+
+// Chatbot
+window.botpressWebChat.init({
+  "composerPlaceholder": "Chat with Pahariya Computers",
+  "botConversationDescription": "",
+  "botId": "dc4ff08d-3e8a-4c8e-86d8-0025d62aa89f",
+  "hostUrl": "https://cdn.botpress.cloud/webchat/v1",
+  "messagingUrl": "https://messaging.botpress.cloud",
+  "clientId": "dc4ff08d-3e8a-4c8e-86d8-0025d62aa89f",
+  "webhookId": "aa70629f-8ea6-4b1c-a102-53b874648089",
+  "lazySocket": true,
+  "themeName": "prism",
+  "frontendVersion": "v1",
+  "showPoweredBy": true,
+  "theme": "prism",
+  "themeColor": "#dd0404",
+  "botName": "Pahariya Computers",
+  // "layoutWidth": "10px",
+  // "containerWidth": "700px",
+  "botConversationDescription": "Let's kick off our conversation with a warm hello!",
+  // "avatarUrl": "Images/logo.png",
+  "useSessionStorage": true,   // to not store the chat history after the session ends on closing the tab
+  "enablePersistHistory": false,
+  // "website": true,
+});
+// Chatbot ends here
