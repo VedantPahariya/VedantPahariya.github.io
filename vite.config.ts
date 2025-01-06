916dd44931b786d207dffa355831b074d726d112
Vedant Pahariya <vedant.pahariya@research.iiit.ac.in>
2025-01-06 15:54:11 +0530

    made changes for correcting error

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/VedantPahariya.github.io/',
+  base: '/',
   optimizeDeps: {
     include: ['lucide-react'], // Change from exclude to include
   },
